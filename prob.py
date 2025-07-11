1d8a01023677f435e7ff38c75753e74c164c8a12
CaputP <167943093+CaputP@users.noreply.github.com>
2025-07-11 20:43:33 +0000

    hola

---
+++ prob.py
@@ -0,0 +1,13 @@
+print("hola mis amigos pendeyvis")
+
+class saludo:
+    def __init__ (self, saludo, despedida):
+        self.saludo = saludo
+        self.despedida = despedida
+    def saldo():
+        print("hola")
+    def despdida():
+        print("adios")
+
+hola=saludo(1, 2)
+hola.saldo()
